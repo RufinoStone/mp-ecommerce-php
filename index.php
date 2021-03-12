10dd0d77389f8610ed4e45568ba0446653e2ae22
Pedro Rufino <60099391+RufinoStone@users.noreply.github.com>
2021-03-12 12:36:46 -0300

    update

--- index.php
+++ index.php
@@ -238,7 +238,7 @@
                                             <div class="as-producttile-title">
                                                 <h3 class="as-producttile-name">
                                                     <p class="as-producttile-tilelink">
-                                                        <span data-ase-truncate="2">Samsung Galaxy S9</span>
+                                                         <span data-ase-truncate="2">Samsung Galaxy S9</span>
                                                     </p>
 
                                                 </h3>
