350a62e88a3a6d56b6052c6cbf3268b5fc72934a
Pedro Rufino <60099391+RufinoStone@users.noreply.github.com>
2021-03-13 17:46:06 -0300

    aaaaaaaaa

--- detail.php
+++ detail.php
@@ -53,6 +53,31 @@
 <body class="as-theme-light-heroimage">
 
 <?php
+  // ...
+  $payer = new MercadoPago\Payer();
+  $payer->name = "Joao";
+  $payer->surname = "Silva";
+  $payer->email = "user@email.com";
+  $payer->date_created = "2018-06-02T12:58:41.425-04:00";
+  $payer->phone = array(
+    "area_code" => "11",
+    "number" => "4444-4444"
+  );
+    
+  $payer->identification = array(
+    "type" => "CPF",
+    "number" => "19119119100"
+  );
+    
+  $payer->address = array(
+    "street_name" => "Street",
+    "street_number" => 123,
+    "zip_code" => "06233200"
+  );
+  // ...
+?>
+
+<?php
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 
@@ -64,9 +89,13 @@ $preference = new MercadoPago\Preference();
 
 // Cria um item na preferência
 $item = new MercadoPago\Item();
-$item->title = 'Meu produto';
-$item->quantity = 1;
-$item->unit_price = 75.56;
+  $item->id = "1234";
+  $item->title = "Heavy Duty Plastic Table";
+  $item->description = "Table is made of heavy duty white plastic and is 96 inches wide and 29 inches tall";
+  $item->category_id = "home";
+  $item->quantity = 7;
+  $item->currency_id = "BRL";
+  $item->unit_price = 75.56;
 $preference->items = array($item);
 $preference->save();
 ?>
