a5164dcdbbdf9b2d3e86eec8b0ecb38fea48f843
Pedro Rufino <60099391+RufinoStone@users.noreply.github.com>
2021-03-13 17:07:00 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -52,6 +52,25 @@ php -r "unlink('composer-setup.php');"
 
 <body class="as-theme-light-heroimage">
 
+<?php
+// SDK de Mercado Pago
+require __DIR__ .  '/vendor/autoload.php';
+
+// Configura credenciais
+MercadoPago\SDK::setAccessToken('PROD_ACCESS_TOKEN');
+
+// Cria um objeto de preferência
+$preference = new MercadoPago\Preference();
+
+// Cria um item na preferência
+$item = new MercadoPago\Item();
+$item->title = 'Meu produto';
+$item->quantity = 1;
+$item->unit_price = 75.56;
+$preference->items = array($item);
+$preference->save();
+?>
+
     <div class="stack">
         
         <div class="as-search-wrapper" role="main">
