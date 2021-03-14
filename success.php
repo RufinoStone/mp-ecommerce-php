708f11a5736d9d9b7e18afa2a09acb2ed1afd2a5
Pedro Rufino <60099391+RufinoStone@users.noreply.github.com>
2021-03-14 19:10:51 -0300

    Update success.php

--- success.php
+++ success.php
@@ -11,7 +11,7 @@
     <script src="https://www.mercadopago.com/v2/security.js" view="success"></script>
     <style>
 div.card {
-  width: 250px;
+  width: 100%;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   text-align: center;
 }
