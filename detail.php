084c4dbcc681ca3cd4902af6ac970d4b8263c372
Pedro Rufino <60099391+RufinoStone@users.noreply.github.com>
2021-03-14 11:03:30 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -8,7 +8,6 @@
     <meta name="format-detection" content="telephone=no">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
           
     <?php
     include("/vendor/autoload.php");
